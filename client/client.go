3ae234aeda7c07cae45a9fb393487a77e4bbb673
ancientmodern <ancientmodern@sjtu.edu.cn>
2021-07-23 17:22:26 +0800

    update client

--- client/client.go
+++ client/client.go
@@ -12,22 +12,27 @@ var num = flag.Int("num", 5, "Input how many times")
 func main() {
 	flag.Parse()
 
-	conn, err := net.DialTimeout("udp", "10.3.39.2:8002", time.Second)
+	conn, err := net.Dial("udp", "10.3.39.2:8002")
 	if err != nil {
 		fmt.Println("net.Dial err:", err)
 		return
 	}
 	defer conn.Close()
 
-	_, err = conn.Write([]byte("0123456789"))
-	if err != nil {
-		fmt.Println("conn.Write err:", err)
+	for i := 0; i < 10; i++ {
+		_, err = conn.Write([]byte("0123456789"))
+		if err != nil {
+			fmt.Println("conn.Write err:", err)
+		}
+
+		buf := make([]byte, 1024)
+		n, err := conn.Read(buf)
+		if err != nil {
+			return
+		}
+		fmt.Println("Received from Server:", string(buf[:n]))
+		time.Sleep(3 * time.Second)
 	}
 
-	buf := make([]byte, 1024)
-	n, err := conn.Read(buf)
-	if err != nil {
-		return
-	}
-	fmt.Println("Received from Server:", string(buf[:n]))
+	fmt.Println("Client ends.")
 }
